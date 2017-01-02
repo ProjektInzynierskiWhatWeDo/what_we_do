56ae6ca8bff52adf2121a69ca64e54c3587f342b
Skałkowski Mateusz <skalkowskimateusz@gmail.com>
2017-01-02 20:12:37 +0300

    test

---
+++ WhatWeDo/app/src/main/java/com/nextbest/skalkowski/whatwedo/activity/SocialLoginActivity.java
@@ -0,0 +1,15 @@
+package com.nextbest.skalkowski.whatwedo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nextbest.skalkowski.whatwedo.R;
+
+public class SocialLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_login);
+    }
+}
